f44457bdee2a14045822f313fc988345e37ca1ef
martinKindall <mart256@gmail.com>
2020-04-28 20:48:45 -0400

    Fighter: created base class to attack and receive damage

--- logic/fighter/Fighter.py
+++ logic/fighter/Fighter.py
@@ -1,11 +1,31 @@
 import abc
 
+from logic.weapon.Weapon import Weapon
+
+
 class Fighter(metaclass=abc.ABCMeta):
 
-	# def __init__(
-	# 		self,
-	# 		maxHitpoints: int,
-	# 		hitpoints: int,
-	# 		weapon
-	# 			 ):
-	pass
+	def __init__(
+			self,
+			name: str,
+			maxHitpoints: int,
+			hitpoints: int,
+			weapon: Weapon,
+			exp: int):
+
+		self.name = name
+		self.maxHitpoints = maxHitpoints
+		self.hitpoints = hitpoints
+		self.weapon = weapon
+		self.exp = exp
+
+	def getAttackPoints(self) -> int:
+		return self.weapon.getAttackPoints()
+
+	def receiveDamage(self, fighter: 'Fighter') -> None:
+		self.hitpoints -= fighter.getAttackPoints()
+		if self.hitpoints < 0:
+			self.hitpoints = 0
+
+	def attack(self, fighter: 'Fighter') -> None:
+		fighter.receiveDamage(self)
