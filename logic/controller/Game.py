039f08b0673ec7640e7183dc57ce1cb8ec2e79b8
martinKindall <mart256@gmail.com>
2020-04-28 21:39:37 -0400

    Game: initiating monsters

--- logic/controller/Game.py
+++ logic/controller/Game.py
@@ -1,6 +1,9 @@
+from typing import List
+
 from rx.subject import Subject
 
 from logic.fighter.Fighter import Fighter
+from logic.fighter.Goblin import Goblin
 from logic.fighter.Player import Player
 
 
@@ -10,3 +13,8 @@ class Game:
 		self.player = player
 		self.gameResult: Subject[bool] = Subject()
 		self.nextFighter: Subject[Fighter] = Subject()
+
+		self.currentMonster: Fighter = Goblin()
+		self.monsters: List[Fighter] = list()
+		self.monsters.append(Goblin())
+		self.monsters.append(Goblin())
