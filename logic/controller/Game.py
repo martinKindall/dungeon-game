b2a00278144bd5c86c753749f9b384893b092418
martinKindall <mart256@gmail.com>
2020-04-28 21:32:07 -0400

    Added observables to Game

--- logic/controller/Game.py
+++ logic/controller/Game.py
@@ -1,3 +1,6 @@
+from rx.subject import Subject
+
+from logic.fighter.Fighter import Fighter
 from logic.fighter.Player import Player
 
 
@@ -5,3 +8,5 @@ class Game:
 
 	def __init__(self, player: Player):
 		self.player = player
+		self.gameResult: Subject[bool] = Subject()
+		self.nextFighter: Subject[Fighter] = Subject()
